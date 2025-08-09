aed8eba97cfe0785bd90c70a4ed5faaeba4108d1
Rao Muhammad Dayan Atif <162185730+dayanatif@users.noreply.github.com>
2025-08-09 06:58:51 +0500

    Time: 0 ms (100%), Space: 17.7 MB (48.36%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,9 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        pre1 = 1
+        pre2 = 0
+        for i in range(1, n+1):
+            cur = pre1 + pre2
+            pre2 = pre1
+            pre1 = cur
+        return pre1
